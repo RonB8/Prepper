5bcfec22e46df68456dd60783f3228d7d01ab5f6
RonB8 <ronbuaron8@gmail.com>
2024-09-25 10:42:54 +0300

    .

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import socket
 
+HOST = '10.100.102.194'  # כתובת ה-IP של השרת (localhost במקרה הזה)
+PORT = 1234  # אותו מספר פורט שבו השרת מאזין
 
+# יצירת חיבור לשרת
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        # שליחת הודעה לשרת
+        message = input("Enter message to send: ")
+        if message.lower() == 'exit':  # יציאה מהלולאה אם הקלט הוא 'exit'
+            break
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("Stam stam")
+        # המרת ההודעה למערך בתים
+        message_data = bytearray(message, 'utf-8')
 
+        # שליחת ההודעה לשרת
+        s.sendall(message_data)
+
+        # קבלת התגובה מהשרת
+        reply_data = s.recv(1024)  # קבלת עד 1024 בתים
+        reply_text = reply_data.decode("utf-8").rstrip('\x00')  # המרת הבתים להודעה
+        print("Received reply from server: " + reply_text)
+
+print("Connection closed.")
--- srv.py
+++ srv.py
@@ -5,4 +5,20 @@ PORT = 1234
 
 # with socket.socket(socket.AF_INET)
 
-print("Try")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            text = data.decode("utf-8")
+            print("Received message: " + text)
+            print("Enter message ")
+            reply = input()
+            replydata = bytearray(reply, "utf-8")
+            newdata = bytearray(1024)
+            for i in range(min(len(replydata), len(newdata))):
+                newdata[i] = replydata[i]
+            conn.sendall(newdata)
